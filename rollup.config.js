f82055c6486c84483cb346b57d2b5c86e5269dbc
Rajiv Seelam <rajiv@betalectic.com>
2021-10-28 10:33:49 +0530

    Try rollup commonjs config

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
 import babel from "@rollup/plugin-babel";
 import postcss from "rollup-plugin-postcss";
 
@@ -13,13 +14,18 @@ export default {
     },
   ],
   preserveModules: true,
-  external: ["react", "react-dom"],
+  external: ["react", "react-dom", "react-dropdowns"],
   plugins: [
     babel({
       babelHelpers: "bundled",
       exclude: "node_modules/**",
     }),
     resolve(),
+    commonjs({
+      // non-CommonJS modules will be ignored, but you can also
+      // specifically include/exclude files
+      include: ["node_modules/@reusejs/**", "node_modules/next/**"],
+    }),
     postcss({
       config: {
         path: "./postcss.config.js",
