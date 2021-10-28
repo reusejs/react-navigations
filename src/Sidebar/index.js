7509f0885fdf3fd75c85e0b405cbeafde9bc317f
Rajiv Seelam <rajiv@betalectic.com>
2021-10-28 22:35:34 +0530

    Add dropdown menu

--- src/Sidebar/index.js
+++ src/Sidebar/index.js
@@ -3,6 +3,7 @@ import "../tailwind.css";
 import { XIcon } from "@heroicons/react/outline";
 import { Dialog, Transition } from "@headlessui/react";
 import { BaseButton } from "@reusejs/react-buttons";
+import { DropdownMenu } from "@reusejs/react-dropdowns";
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
@@ -24,6 +25,15 @@ const Sidebar = function ({
     <>
       <div>Helloo</div>
       <BaseButton>Hello Button</BaseButton>
+      <DropdownMenu
+        profilePictureUrl={
+          "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+        }
+        teams={[
+          { name: "Beta", href: "#", current: false },
+          { name: "Betalectic", href: "#", current: true },
+        ]}
+      />
     </>
   );
 };
